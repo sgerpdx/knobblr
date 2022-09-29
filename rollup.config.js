ceae19520ad3033533cfe00475635c9bd87b992f
SamuelHGerber <henrylightfoot@gmail.com>
2022-09-28 19:08:22 -0700

    testing dependencies and configs

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -26,6 +28,8 @@ export default [
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
       postcss(),
+      peerDepsExternal(),
+      terser(),
     ],
   },
   {
