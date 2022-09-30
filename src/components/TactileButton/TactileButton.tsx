c210e675d288ecf5c83bd9be02785b04678b241d
SamuelHGerber <henrylightfoot@gmail.com>
2022-09-30 09:45:15 -0700

    TactileButton props interface update

--- src/components/TactileButton/TactileButton.tsx
+++ src/components/TactileButton/TactileButton.tsx
@@ -3,10 +3,15 @@ import "./TactileButton.css";
 
 export interface TactileButtonProps {
   width: number;
+  height?: number; //optional: defaults to [width/ * (3/4)]
   fillColor: string;
   strokeColor: string;
+  mode?: string; //one of: *rubber, plastic
   label: string;
-  mode: string;
+  labelPosition?: string; //one of: *on, above, below
+  lightingDirection?: number; //takes a number between 0 and 360 (degrees)
+  travel?: string; //optional: governs downward travel of button on click (def = ___)
+  sound?: boolean; //defaults to false; click sound for additional sensory feedback
 }
 
 const TactileButton = (props: TactileButtonProps) => {
